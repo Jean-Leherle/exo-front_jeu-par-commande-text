b2210b072b89c84d37a05a1de890acf4d55c86c5
Jean-Leherle <j.leherle@gmail.com>
2022-08-01 19:00:38 +0200

    etape 1.1 , 1.2 terminé. utilisation d'une précision de classesur chaque cellule

--- js/app.js
+++ js/app.js
@@ -1,22 +1,48 @@
 var app = {
-  init: function() {
-    console.log('init');
+  init: function () {
 
     // TODO
     app.drawBoard();
 
     // Event listeners - TODO
   },
-  handleLaunchScriptButton: function() {
+  param: {
+    nbRow: 4,
+    nbColumn:6
+  },
+
+  drawBoard: function () {
+    const board = document.getElementById('board')
+    const row = []
+    for (let r = 0; r < app.param.nbRow; r++) {
+      row.push(document.createElement('div'))
+      row[r].classList.add("cellRow")
+      row[r].id = 'row' + (r + 1)
+      board.appendChild(row[r])
+      const column = []
+      for (let c = 0; c < app.param.nbColumn; c++) {
+        column.push(document.createElement('div'))
+        column[c].classList.add("cell")
+        column[c].classList.add('r' + (r+1),'c' + (c+1) )
+        row[r].appendChild(column[c])
+      }
+    }
+
+    document.querySelector('.c1.r1').classList.add('cellStart')
+    document.querySelector(`.r${app.param.nbRow}.c${app.param.nbColumn}`).classList.add('cellEnd')
+
+  },
+
+  handleLaunchScriptButton: function () {
     // TODO
-    
+
     // TODO : get all lines as an array
 
-    window.setTimeout(function() {
+    window.setTimeout(function () {
       app.codeLineLoop(codeLines, 0);
     }, 2000);
   },
-  codeLineLoop: function(codeLines, index) {
+  codeLineLoop: function (codeLines, index) {
     // Getting currentLine
     var currentLine = codeLines[index];
     console.log(currentLine);
@@ -28,16 +54,16 @@ var app = {
     // if still a line to interpret
     if (index < codeLines.length) {
       // Recall same method (=> make a loop)
-      window.setTimeout(function() {
+      window.setTimeout(function () {
         app.codeLineLoop(codeLines, index);
       }, 1000);
     } else {
-      window.setTimeout(function() {
+      window.setTimeout(function () {
         app.checkSuccess();
       }, 1000);
     }
   },
-  checkSuccess: function() {
+  checkSuccess: function () {
     // TODO display if the game is won or not
   }
 };
