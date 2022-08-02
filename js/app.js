8b3900887bf1fb496b18b12a8444a692ec3a46cd
Jean-Leherle <j.leherle@gmail.com>
2022-08-02 18:41:32 +0200

    etape 2 : avance dans tous les sens possible

--- js/app.js
+++ js/app.js
@@ -7,8 +7,9 @@ var app = {
     // Event listeners - TODO
   },
   param: {
+    directionList: ['cellCurrent-right', 'cellCurrent-bottom', 'cellCurrent-left', 'cellCurrent-top'],
     nbRow: 4,
-    nbColumn:6
+    nbColumn: 6,
   },
 
   drawBoard: function () {
@@ -23,7 +24,7 @@ var app = {
       for (let c = 0; c < app.param.nbColumn; c++) {
         column.push(document.createElement('div'))
         column[c].classList.add("cell")
-        column[c].classList.add('r' + (r+1),'c' + (c+1) )
+        column[c].classList.add('r' + (r + 1), 'c' + (c + 1))
         row[r].appendChild(column[c])
       }
     }
@@ -31,6 +32,76 @@ var app = {
     board.querySelector('.c1.r1').classList.add('cellStart')
     board.querySelector(`.r${app.param.nbRow}.c${app.param.nbColumn}`).classList.add('cellEnd')
     board.querySelector('.cellStart').classList.add('cellCurrent')
+  },
+
+  moveForward: function () {
+    const actualPosition = document.querySelector('.cellCurrent')
+    let direction
+    for (directionTested of app.param.directionList) {
+      if (actualPosition.classList.contains(directionTested)) {
+        direction = directionTested
+      }
+    }
+
+    const rowElement = actualPosition.closest('.cellRow')
+    const rowElementList = rowElement.querySelectorAll('.cell')
+    switch (direction) {
+      case app.param.directionList[1]: //bottom
+        console.log(app.param.directionList[1], direction);
+        rowElementList.forEach((element, key) => {
+          if ((element === actualPosition) && (parseInt(rowElement.id[3]) !== app.param.nbRow)) {
+            actualPosition.classList.remove('cellCurrent', app.param.directionList[1])
+            document.getElementById('row' + (parseInt(rowElement.id[3]) + 1))
+              .querySelectorAll('.cell')[key]
+              .classList.add('cellCurrent', app.param.directionList[1])
+          }
+        })
+
+        break
+
+      case app.param.directionList[2]://left
+        console.log(app.param.directionList[2], direction);
+
+        rowElementList.forEach((element, key) => {
+          if ((element === actualPosition) && ((key) !== 0)) {
+            actualPosition.classList.remove('cellCurrent', app.param.directionList[2])
+            rowElementList[key - 1].classList.add('cellCurrent', app.param.directionList[2])
+          }
+        })
+        break
+
+      case app.param.directionList[3]: //top
+        console.log(app.param.directionList[3], direction);
+
+        rowElementList.forEach((element, key) => {
+          if ((element === actualPosition) && ((parseInt(rowElement.id[3]) - 1) !== 0)) {
+            actualPosition.classList.remove('cellCurrent', app.param.directionList[3])
+            document.getElementById('row' + (parseInt(rowElement.id[3]) - 1))
+              .querySelectorAll('.cell')[key]
+              .classList.add('cellCurrent', app.param.directionList[3])
+          }
+        })
+        break
+
+      default: //right
+        console.log(app.param.directionList[0], direction);
+        rowElementList.forEach((element, key) => {
+          if ((element === actualPosition) && ((key + 1) !== app.param.nbColumn)) {
+            actualPosition.classList.remove('cellCurrent', app.param.directionList[0])
+            rowElementList[key + 1].classList.add('cellCurrent', app.param.directionList[0])
+          }
+        })
+    }
+
+  },
+
+  turnRight: function () {
+    const actualPosition = document.querySelector('.cellCurrent')
+    app.param.directionList.forEach((direction, key) => {
+      if (actualPosition.classList.contains(directionTested)) {
+        direction = directionTested
+      }
+    })
   },
 
   handleLaunchScriptButton: function () {
